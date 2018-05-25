3822c61274b2872f85fdaa9a5fdd66f34f8398c4
witt <447557027@qq.com>
2018-05-25 12:19:12 +0800

    add readme file

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # ali-oss-uploader
 auto upload static file to aliyun oss
+
+# how to use
+
+## 1. load deps
+```shell
+composer install
+```
+## 2. set config file 
+rename lib/config_demo.php to lib/Config.php
+
+## 3. upload
+
+```shell
+php index.php
+```
---
+++ lib/Config_demo.php
@@ -0,0 +1,18 @@
+<?php
+namespace lib;
+class Config{
+    static $accessKeyId = 'your id';
+    static $accessKeySecret = 'your secret';
+    static $endpoint = 'your endpoint';
+    static $bucket = 'your bucket name';
+    static $oss_basepath = '';//'','app/'
+    static $ignore = [
+        '.git',
+        '.idea',
+        'bower_components',
+        '.gitignore',
+        'bower.json',
+        'other file or path'
+    ];
+    static $source = 'C:\Users\Administrator\Desktop\your_path';
+}
